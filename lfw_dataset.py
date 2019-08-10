82375ba2e8ccc9153ea388246d95c75fb6a048e7
Rayleigh_Kim <wood.rayleigh@gmail.com>
2019-08-10 16:42:07 +0900

    Update lfw_dataset.py

--- lfw_dataset.py
+++ lfw_dataset.py
@@ -5,6 +5,49 @@ import pandas as pd
 import tarfile
 import tqdm
 
+class SimpleTqdm():
+    def __init__(self, iterable=None, total=None, **kwargs):
+        self.iterable = list(iterable) if iterable is not None else None
+        self.total = len(self.iterable) if self.iterable is not None else total
+        assert self.iterable is not None or self.total is not None
+        self.current_step = 0
+        self.print_frequency = max(self.total // 50, 1)
+        self.desc = ""
+
+    def set_description_str(self, desc):
+        self.desc = desc
+
+    def set_description(self, desc):
+        self.desc = desc
+
+    def update(self, steps):
+        last_print_step = (self.current_step // self.print_frequency) * self.print_frequency
+        i = 1
+        while last_print_step + i * self.print_frequency <= self.current_step + steps:
+            print("*", end='')
+            i += 1
+        self.current_step += steps
+
+    def close(self):
+        print("\n" + self.desc)
+
+    def __iter__(self):
+        assert self.iterable is not None
+        self.index = 0
+        return self
+
+    def __next__(self):
+        if self.index < self.total:
+            element = self.iterable[self.index]
+            self.update(1)
+            self.index += 1
+            return element
+        else:
+            self.close()
+            raise StopIteration
+
+
+
 ATTRS_NAME = "lfw_attributes.txt"  # http://www.cs.columbia.edu/CAVE/databases/pubfig/download/lfw_attributes.txt
 IMAGES_NAME = "lfw-deepfunneled.tgz"  # http://vis-www.cs.umass.edu/lfw/lfw-deepfunneled.tgz
 RAW_IMAGES_NAME = "lfw.tgz"  # http://vis-www.cs.umass.edu/lfw/lfw.tgz
